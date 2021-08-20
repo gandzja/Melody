421089c3668b4737ce89bd7fbef0d90c14b2a046
gandzja <48655755+gandzja@users.noreply.github.com>
2021-08-20 16:08:31 +0300

    3-й

--- js/main.js
+++ js/main.js
@@ -2,19 +2,47 @@ $(document).ready(function () {
 	var currentFloor = 2;
 	var counterUp = $(".counter-up");
 	var counterDown = $(".counter-down");
-	var floorPath = $(".home-img path")
+	var floorPath = $(".home-img path");
+	var modal = $(".modal");
+	var modalCloseButton = $(".modal-close-button");
+	var viewFlatsButton = $(".view-flats");
+	var currentFlats = 1; // переменная с текущей квартирой
+  var flatsPath = $(".flats path"); // квартиры SVG
+	var flatsPathItem = $(".flat-item "); // характеристики квартир
+
+	flatsPath.on("mouseover", function () {
+        currentFlats = $(this).attr("data-flats"); // записываем текущее значение в переменную с квартирами
+        flatsPath.removeClass("current-flats"); // удаляем класс квартир
+        flatsPathItem.removeClass("current-flats-item"); // удаляем класс характеристик квартиры
+        $(`[data-flats=${currentFlats}]`).toggleClass("current-flats"); // добавляем класс квартиры
+        $(`[data-item=${currentFlats}]`).toggleClass("current-flats-item"); // добавляем класс характеристик квартиры
+	})
+
+	 flatsPathItem.on("mouseover", function () {
+        currentFlats = $(this).attr("data-item"); // записываем текущее значение в переменную с квартирами
+        flatsPath.removeClass("current-flats"); // удаляем класс квартир
+        flatsPathItem.removeClass("current-flats-item"); // удаляем класс характеристик квартиры
+        $(`[data-flats=${currentFlats}]`).toggleClass("current-flats"); // добавляем класс квартиры
+        $(`[data-item=${currentFlats}]`).toggleClass("current-flats-item"); // добавляем класс характеристик квартиры
+    })
+
 	//при наведении мышкой на этаж
 	floorPath.on("mouseover", function () {
 		currentFloor = $(this).attr("data-floor");
 		floorPath.removeClass("current-floor");
 		$(".counter").text(currentFloor);
 	});
+	viewFlatsButton.on("click", toggleModal);
 		// при нажатии на этаж
 		floorPath.on("click", function () {
 		currentFloor = $(this).attr("data-floor");
 		floorPath.removeClass("current-floor");
-		$(".counter").text(currentFloor);
-	});
+		$(".counter").text(currentFloor); // открываем модальное окно
+		});
+
+	floorPath.on("click", toggleModal);
+	//закрываем модальное окно
+	modalCloseButton.on("click", toggleModal);
 	// при нажатии кнопки вверх
 	counterUp.on("click", function () {
 		if (currentFloor < 18) {
@@ -34,5 +62,9 @@ $(document).ready(function () {
 			floorPath.removeClass("current-floor");
 			$(`[data-floor=${usCurrentFloor}]`).toggleClass("current-floor");
 		}
-	});
+		});
+	// функция открыть - закрыть окно
+		function toggleModal () {
+			modal.toggleClass("is-open");
+		}
 });
